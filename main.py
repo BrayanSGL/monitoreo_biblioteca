1eb0a145cfdea8395c97d705a06b037f633ca3e2
BrayanSGL <bsgaleanol@udistrital.edu.co>
2023-06-03 19:45:34 -0500

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hola mundo')
+import redis 
+import time
+
+r = redis.Redis(
+    host ='redis-15497.c44.us-east-1-2.ec2.cloud.redislabs.com',
+    port=15497,
+    password='nQ8NJJBVdusp18Y7o5ApPbUirlO5NshT')
+
+ts = r.ts()
+ts.create("pureba")
+
+for i in range(100):
+    ts.add("prueba", "*",i)
+    time.sleep(1)
+    
